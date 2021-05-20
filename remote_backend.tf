88513db564e68bc655ab2b619e846ed1d27f6ff2
NandanRao <nandan.pv@gmail.com>
2021-05-20 16:29:02 +0000

    Test

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,7 +1,7 @@
 # If you are in a workshop...
 # Do not delete this file!
 # It's required to complete the Instruqt labs.
-
+#testing
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
